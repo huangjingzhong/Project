935576064d27d2832d02e32ee624f69f897f6235
huangjingzhong <2939497645@qq.com>
2016-08-06 12:11:11 +0800

    add a

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 * @Date:   2016-08-06 12:09:08
 * @Last Modified by:   anchen
 * @Last Modified time: 2016-08-06 12:09:17
+* @Last Modified time: 2016-08-06 12:10:30
 */
 
 #include <stdio.h>
@@ -10,5 +11,7 @@
 int main() 
 {
 
+    int a =100;
+    printf("a= %d\n",a);
     return 0;
 }
