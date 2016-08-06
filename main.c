c7b5dea26bdac898c12fe28acbc7e01d10f8cb7c
huangjingzhong <2939497645@qq.com>
2016-08-06 12:20:13 +0800

    add  a+b

--- main.c
+++ main.c
@@ -4,17 +4,18 @@
 * @Last Modified by:   anchen
 * @Last Modified time: 2016-08-06 12:14:20
 * @Last Modified time: 2016-08-06 12:17:00
+* @Last Modified time: 2016-08-06 12:19:12
 */
 
 #include <stdio.h>
 
 int main() 
 {
-
     int a =100;
     printf("a= %d\n",a);
      int b =100;
     printf("b= %d\n",b);
+    printf("%d\n", a+b);
     printf("hello world!!\n");
     return 0;
 }
